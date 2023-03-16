aee7537c191fc0cc8ce199e61961c6f631b74e55
Mh291093 <mohamedhassanabdelhady93@gmail.com>
2023-03-16 13:21:28 -0700

    fourth task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ * main: Entry point
+ *
+ * Description: A C program which prints by puts function
+ * 
+ * return: always 0 which means success
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ * main: Entry point
+ *
+ * Description: A C program which prints by printf function
+ *
+ * return: always 0 which means success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return 0;
+}
