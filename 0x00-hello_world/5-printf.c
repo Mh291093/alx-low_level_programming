93bad68b2ea39e3a4d043eecf2095250de790729
Mh291093 <mohamedhassanabdelhady93@gmail.com>
2023-03-16 13:49:56 -0700

    fifth task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /*
- * main: Entry point
+ * main - Entry point
  *
  * Description: A C program which prints by printf function
  *
- * return: always 0 which means success
- */
+ * Return: always 0 (success)
+*/
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+	return (0);
 }
