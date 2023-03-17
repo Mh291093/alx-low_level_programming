5f280f98b5635ae92e75c12af6d343a4cd2b3ba3
Mh291093 <mohamedhassanabdelhady93@gmail.com>
2023-03-17 10:57:47 -0700

    task-09

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program which prints numbers of base 16
+ *
+ * Return: always 0 (success)
+*/
+
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (m = 97; m = 102; m++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}
