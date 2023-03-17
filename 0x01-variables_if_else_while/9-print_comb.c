c9255a402c8e4ee1bdd3c623b9bb597514b38996
Mh291093 <mohamedhassanabdelhady93@gmail.com>
2023-03-17 11:03:25 -0700

    task-10

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program which prints by printf function
+ *
+ * Return: always 0 (success)
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
