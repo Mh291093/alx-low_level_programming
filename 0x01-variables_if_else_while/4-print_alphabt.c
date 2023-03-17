e3591fc1a05d796d501ba5b3d823fbdba06a55f4
Mh291093 <mohamedhassanabdelhady93@gmail.com>
2023-03-17 10:30:56 -0700

    When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program which prints by printf function
+ *
+ * Return: always 0 (success)
+*/
+
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		if (n == 101 || n == 113)
+		{
+			continue;
+			n++;
+		}
+		putchar(n);
+		n++;
+	}
+	return (0);
+}
