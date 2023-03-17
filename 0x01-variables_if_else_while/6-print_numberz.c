2b60ddb9bdb06a273e0e9ab91f67af6a1e192866
Mh291093 <mohamedhassanabdelhady93@gmail.com>
2023-03-17 10:47:39 -0700

    task-06

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program which prints by printf function
+ *
+ * Return: always 0 (success)
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n + 48);
+	}
+	putchar('\n');
+	return (0);
+}
