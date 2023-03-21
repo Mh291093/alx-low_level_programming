cd300fb18557486c1274e5eec8742fb509da2549
Mh291093 <mohamedhassanabdelhady93@gmail.com>
2023-03-21 14:23:03 -0700

    08

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,27 +7,18 @@
 
 void jack_bauer(void)
 {
-	int a, b, c, d;
+	int min, hr;
 
-	for (a = 0; a <= 2; a++)
+	for (hr = 0; hr <= 23; hr++)
 	{
-		for (b = 0; b <= 9; b++)
+		for (min = 0; min <= 23; min++)
 		{
-			if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
-			{
-				for (c = 0; c <= 5; c++)
-				{
-					for (d = 0; d <= 9; d++)
-					{
-						_putchar(a + '0');
-						_putchar(b + '0');
-						_putchar(50);
-						_putchar(c + '0');
-						_putchar(d + '0');
-						_putchar('\n');
-					}
-				}
-			}
+			_putchar((hr / 10) + 48);
+			_putchar((hr % 10) + 48);
+			_putchar(':');
+			_putchar((min / 10) + 48);
+			_putchar((min % 10) + 48);
+			_putchar('\n');
 		}
 	}
 }
