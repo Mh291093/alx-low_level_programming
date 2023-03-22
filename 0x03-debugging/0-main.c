524f5b57346ce1d43a9a37bddfc336f4495b6e00
Mh291093 <mohamedhassanabdelhady93@gmail.com>
2023-03-22 08:29:06 -0700

    00

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -5,27 +5,11 @@
 * Return: 0
 */
 
-int positive_or_negative(int n)
-{
-	if (n > 0)
-	{
-		printf("%i is positive\n", n);
-	}
-	else if ( n == 0)
-	{
-		printf("%i is zero\n", n);
-	}
-	else
-	{
-		printf("%i is negative\n", n);
-	}
-}
-
 int main(void)
 {
 	int i;
 
-	i = 0;
+	i = 98 % 2;
 	positive_or_negative(i);
 	return (0);
 }
