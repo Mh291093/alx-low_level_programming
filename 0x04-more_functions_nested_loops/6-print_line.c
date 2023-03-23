98f5ebf180f1fe165a2a6f1221c2df071c708c75
Mh291093 <mohamedhassanabdelhady93@gmail.com>
2023-03-23 06:39:09 -0700

    6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -5,19 +5,23 @@
  *
  * @n: number times
  *
- * Return: 1 if its upper, 0 if not
+ * Return:
  */
 
 void print_line(int n)
 {
-	int ln_chr;
+	int lnchr;
 
 	if (n <= 0)
+	{
 		_ptchar('\n');
+	}
 	else
 	{
-		for (ln_chr = 1; ln_chr <= n; ln_chr++)
+		for (lnchr = 1; lnchr <= n; lnchr++)
+		{
 			_putchar('_');
+		}
 		_putchar('\n');
 	}
 }
