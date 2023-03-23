6057879f8ccc5cca3917de4dcb0f6d87ca4348c8
Mh291093 <mohamedhassanabdelhady93@gmail.com>
2023-03-23 06:52:06 -0700

    7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,7 +5,7 @@
  *
  *@n: input
  *
- * Return: 1 if its upper, 0 if not
+ * Return: empty
  */
 
 void print_diagonal(int n)
@@ -13,13 +13,17 @@ void print_diagonal(int n)
 	int postn, space;
 
 	if (n <= 0)
+	{
 		_putchar('\n');
+	}
 	else
 	{
-		for (postn = 1; postn <= n; postn++)
+		for (postn = 0; postn < n; postn++)
 		{
-			for (space = 1; space <= postn; space++)
-				_putchar(' ');
+			for (space = 0; space < postn; space++)
+			{
+				_putchar(32);
+			}
 			_putchar(92);
 			_putchar('\n');
 		}
