1e319cbd00289ec8ecd48fe2539aa2e79954e29f
Mh291093 <mohamedhassanabdelhady93@gmail.com>
2023-04-05 15:00:01 -0700

    task5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - a function
+ *
+ * @n: input
+ *
+ * @val: input
+ *
+ * Return: return (n)
+ */
+
+int square(int n, int val);
+int _sqrt_recursion(int n)
+{
+	return (square(n, 1));
+}
+
+/**
+ * square - a function
+ *
+ * @n: input
+ *
+ * @val: input
+ *
+ * Return: return
+ */
+
+int square(int n, int val)
+{
+	if (val * val == n)
+	{
+		return (val);
+	}
+	else if (val * val > n)
+	{
+		return (square(n, val + 1));
+	}
+	else
+	{
+		return (-1);
+	}
+}
